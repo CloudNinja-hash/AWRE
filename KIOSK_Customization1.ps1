884adb43551429c3e078b3c1ece35f002fcbea1d
CloudNinja-hash <andrew.luan@anywhere.re>
2025-04-23 12:35:03 -0500

    Update KIOSK_Customization1.ps1

--- KIOSK_Customization1.ps1
+++ KIOSK_Customization1.ps1
@@ -207,7 +207,7 @@ Function InstallIvanti () {
     Start-Process -FilePath msiexec.exe -ArgumentList "/i `"$extractedFilePath`" /qn" -Wait -Verbose
 
     Write-Output "Waiting for Ivanti Agent install to complete...", ""
-    Start-Sleep -Seconds 30
+    Start-Sleep -Seconds 15
 
     Write-Output "Ivanti Agent install complete...", ""
 
