cb78676667a8b14d0a8a0f461ed6196a164c3b18
CloudNinja-hash <andrew.luan@anywhere.re>
2025-04-23 09:52:45 -0500

    Update KIOSK_Customization.ps1

--- KIOSK_Customization.ps1
+++ KIOSK_Customization.ps1
@@ -1,8 +1,6 @@
-# Parameter for SetupAutologon function
-param($Password)
 
 # Log file location for script
-$logfilePath = "C:\LOG_Kiosk_Customization.txt" 
+$logfilePath = "C:\LOG_Kiosk_Customization1.txt" 
 
 # Variables for creating desktop shortcuts for Edge/Chrome
 $HomepageURL = 'https://mycbdesk.com'
@@ -337,7 +335,38 @@ Function SetupRestart () {
     Write-Output "File copied successfully to C:\Users\Public\Desktop"
 
 }
+<##
+# Function to Setup Autologin
+Function SetupAutologin () {
 
+    $securePWD = ConvertTo-SecureString $Password -AsPlainText -Force
+
+    # Define the URL of the download file and the destination path
+    $downloadUrl = "https://dl.dropboxusercontent.com/scl/fi/7grf0rhauao2e0xiust40/Autologon64.exe?rlkey=v2i49l93acyuhlkp1t1fggrpl&st=vqs1m2u3"
+    $destinationPath = "C:\Windows\Temp\Autologon64.exe"
+
+    # Download the file
+    Write-Output "Downloading Autologon64.exe...", ""
+    Invoke-WebRequest -Uri $downloadUrl -OutFile $destinationPath -Verbose
+
+    # Get the Agent account
+    Write-Output "Getting Agent account info...", ""
+    $localAcct = Get-LocalUser -Name "Agent"
+
+    # Change the password
+    Write-Output "Changing Agent password...", ""
+    $localAcct | Set-LocalUser -Password $securePWD -AccountNeverExpires -PasswordNeverExpires $true -Verbose
+
+    Start-Sleep -Seconds 30
+
+    Write-Output "Updating Autologon settings...", ""
+    Start-Process -FilePath $destinationPath -ArgumentList "/accepteula","Agent","WorkGroup",$Password
+
+    $securePWD = $null
+    $Password = $null
+
+}
+##>
 # Function to install Chrome/Edge ADMX files to disable First-Time run and additional shortcuts in C:\Users\Agent\Desktop directory
 Function InstallADMX () {
 
@@ -553,36 +582,14 @@ Write-Output "Refresh the Start menu to apply changes...", ""
 Stop-Process -Name explorer -Force -Verbose
 Start-Process explorer -Verbose
 
+<##
+# Run the function to set up Autologin
+SetupAutologin
+##>
+
 # Update registry to the latest Kiosk version
 New-Item -Path "HKLM:\SOFTWARE\ImageVersion" -Force -Verbose
 New-ItemProperty -Path "HKLM:\SOFTWARE\ImageVersion" -Name "Agent Workstation" -PropertyType String -Value "Agent 2025 V2.0" -Force -Verbose
-
-# Convert String to Secure String
-$securePWD = ConvertTo-SecureString $Password -AsPlainText -Force
-
-# Define the URL of the download file and the destination path
-$downloadUrl = "https://dl.dropboxusercontent.com/scl/fi/7grf0rhauao2e0xiust40/Autologon64.exe?rlkey=v2i49l93acyuhlkp1t1fggrpl&st=vqs1m2u3"
-$destinationPath = "C:\Windows\Temp\Autologon64.exe"
-
-# Download the file
-Write-Output "Downloading Autologon64.exe...", ""
-Invoke-WebRequest -Uri $downloadUrl -OutFile $destinationPath -Verbose
-
-# Get the Agent account
-Write-Output "Getting Agent account info...", ""
-$localAcct = Get-LocalUser -Name "Agent"
-
-# Change the password
-Write-Output "Changing Agent password...", ""
-$localAcct | Set-LocalUser -Password $securePWD -AccountNeverExpires -PasswordNeverExpires $true -Verbose
-
-Start-Sleep -Seconds 60
-
-Write-Output "Updating Autologon settings...", ""
-Start-Process -FilePath $destinationPath -ArgumentList "/accepteula","Agent","WorkGroup",$Password
-
-$securePWD = $null
-$Password = $null
 
 ## End logging
 Stop-Transcript | Out-Null
