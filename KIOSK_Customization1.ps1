1b339bb63c3b791a161a4ce084abba156a9d70e1
CloudNinja-hash <andrew.luan@anywhere.re>
2025-08-20 13:53:43 -0500

    Update KIOSK_Customization1.ps1

--- KIOSK_Customization1.ps1
+++ KIOSK_Customization1.ps1
@@ -670,10 +670,7 @@ Start-Process explorer -Verbose
 New-Item -Path "HKLM:\SOFTWARE\ImageVersion" -Force -Verbose
 New-ItemProperty -Path "HKLM:\SOFTWARE\ImageVersion" -Name "Agent Workstation" -PropertyType String -Value "Agent 2025 V2.0" -Force -Verbose
 
-Start-Sleep -Seconds 5
-
 ## End logging
 Stop-Transcript | Out-Null
 
 Restart-Computer -Force
-
